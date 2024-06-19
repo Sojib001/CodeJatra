c2e61481d1b174da9ab4278ec919370be34967c4
bahar0900 <sagormdsagorchowdhury@gmail.com>
2024-06-19 16:19:24 +0600

    democommit

--- landing page/landingpage.php
+++ landing page/landingpage.php
@@ -22,7 +22,7 @@
 
     <nav class="navbar navbar-expand-lg bg-white sticky-top">
         <div class="container">
-            <a class="navbar-brand" href="landingpage.html">
+            <a class="navbar-brand" href="landingpage.php">
                 <img src="image/LOGOBRIGHT.jpg" alt="notfound" style="max-height: 50px; max-width: 50px;">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
